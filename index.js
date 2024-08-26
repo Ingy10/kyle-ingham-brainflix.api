03433598869066f497ec06d93fb964c6f631faf7
Kyle Ingham <kyleingy@proton.me>
2024-08-26 17:24:16 -0600

    Retrieved video data from json file and routed a get request to the list of video info.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import videoRouter from "./routes/videos.js";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/videos", videoRouter);
 
 app.get("/", (_request, response) => {
   response.send("Hello World!");
 });
 
-app.listen(8080, () => {
-  console.log("Successfully listening on 8080");
+app.listen(PORT, () => {
+  console.log(`Successfully listening on ${PORT}`);
 });
---
+++ routes/videos.js
@@ -0,0 +1,22 @@
+import express from "express";
+const router = express.Router();
+import fs from "fs";
+
+const path = "./data/videos.json";
+
+const readData = () => {
+  const videoData = fs.readFileSync(path);
+  const parsedVideoData = JSON.parse(videoData);
+  //   console.log(videoData);
+  console.log(parsedVideoData[0].title);
+  return parsedVideoData;
+};
+
+readData();
+
+router.get("/videos", (req, res) => {
+  const videoListData = readData();
+  res.status(200).json(videoListData);
+});
+
+export default router;
