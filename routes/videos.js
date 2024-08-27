9619d91b2bb889a59e311baf1a8e65b3df62de3e
Kyle Ingham <kyleingy@proton.me>
2024-08-27 11:12:50 -0600

    Created routes to get video data.  Confirmed successful connection using get requests with client side to get a list of video details as well as any video in the list by its ID.

--- routes/videos.js
+++ routes/videos.js
@@ -5,11 +5,16 @@ import fs from "fs";
 const path = "./data/videos.json";
 
 const readData = () => {
-  const videoData = fs.readFileSync(path);
-  const parsedVideoData = JSON.parse(videoData);
-  //   console.log(videoData);
-  console.log(parsedVideoData[0].title);
-  return parsedVideoData;
+  try {
+    const videoData = fs.readFileSync(path);
+    const parsedVideoData = JSON.parse(videoData);
+    console.log(videoData);
+    console.log(parsedVideoData);
+    return parsedVideoData;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 readData();
@@ -24,6 +29,20 @@ router.get("/", (req, res) => {
   }));
   console.log(videoListData);
   res.status(200).json(videoListData);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const videoListData = readData();
+
+  const chosenVideo = videoListData.find((video) => video.id === id);
+  console.log(chosenVideo);
+
+  if (!chosenVideo) {
+    res.status(404).send("Video not found");
+  } else {
+    res.status(200).json(chosenVideo);
+  }
 });
 
 export default router;
